ab30479236c850fdf336c682e37f1c4cca4efb65
yassineaouadi08-hash <yassineaouadi08@gmail.com>
2026-08-29 13:57:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def home():
 def health():
     return "OK"
 
+port = int(os.environ.get("PORT", 10000))
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
